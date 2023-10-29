c100adf52849266d34f466025fd0fd5d4f2b2840
Parth Agrawal <63506254+parthagrawal02@users.noreply.github.com>
2023-10-29 22:29:27 +0530

    AUC

--- engine_finetune.py
+++ engine_finetune.py
@@ -139,12 +139,16 @@ def evaluate(data_loader, model, device, args):
             metric_logger.meters['acc5'].update(acc5.item(), n=batch_size)
         else:
             acc1 = accuracy_score(target.cpu(), torch.sigmoid(output.cpu()) > 0.5)*100
-            auc = roc_auc_score(target.cpu(), torch.sigmoid(output.cpu()) > 0.5, average='macro')
+            try:
+                auc = roc_auc_score(target.cpu(), torch.sigmoid(output.cpu()) > 0.5, average='macro')
+            except ValueError:
+                auc = 0
+
             batch_size = images.shape[0]
             metric_logger.update(loss=loss.item())
             metric_logger.meters['acc1'].update(acc1.item(), n=batch_size)
             metric_logger.meters['auc'].update(auc.item(), n=batch_size)
-            
+
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print('* Acc@1 {top1.global_avg:.3f} loss {losses.global_avg:.3f}'
