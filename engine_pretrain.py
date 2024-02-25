b59ec41909c22261f3edf9bec4a9aebf3b08af70
Parth Agrawal <63506254+parthagrawal02@users.noreply.github.com>
2024-02-25 17:02:15 +0530

    Logging

--- engine_pretrain.py
+++ engine_pretrain.py
@@ -37,7 +37,8 @@ def train_one_epoch(model: torch.nn.Module,
     if log_writer is not None:
         print('log_dir: {}'.format(log_writer.log_dir))
 
-    for data_iter_step, (samples,_) in enumerate(data_loader):
+    for data_iter_step, (samples, _) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+
 
         # we use a per iteration (instead of per epoch) lr scheduler
         if data_iter_step % accum_iter == 0:
